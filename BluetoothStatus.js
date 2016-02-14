d73c78747b8389b92c931596877117b8a4c65a02
pknoe3lh <pknoe3lh@users.noreply.github.com>
2016-02-15 00:01:24 +0100

    Update BluetoothStatus.js

--- BluetoothStatus.js
+++ BluetoothStatus.js
@@ -23,7 +23,6 @@ BluetoothStatus.makeVisible = function() {
 };
 ;
 
-
 BluetoothStatus.setName = function() {
     exec(null, null, "BluetoothStatus", "setName", []);
 };
@@ -31,7 +30,18 @@ BluetoothStatus.setName = function() {
 BluetoothStatus.bt = function(sucess,error) {
     exec(sucess, error, "BluetoothStatus", "bt", Array.prototype.slice.call(arguments,2));
 }
+BluetoothStatus.eventlist=[];
+BluetoothStatus.btaddevent = function(type,callback) {
+    luetoothStatus.eventlist.push([type,callback]);
+}
 
+BluetoothStatus.btevent = function(type,data){
+    BluetoothStatus.eventlist.forEach(function (event){
+        if(event[0]==type){
+            event[1](data);
+        }        
+    });    
+}
 
 BluetoothStatus.hasBT = false;
 BluetoothStatus.hasBTLE = false;
